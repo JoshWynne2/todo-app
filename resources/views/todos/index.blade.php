8f7466aa9a7d636d08b4e0e3d890e46c9b5c3ba6
JoshWynne2 <N00221586@student.iadt.ie>
2023-10-09 11:55:47 +0100

    views and seeds all over the place

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Todos</h2>
+
+<table>
+	<thead> 
+		<th>Title</th>
+		<th>Snippet</th>
+		<th>Created</th>
+		<th></th>
+	</thead>
+	<tbody> 
+
+		@forelse ($todos as $todo)
+			<tr>
+				<td>{{ $todo->title }}</td>
+				<td>{{ $todo->body }}</td>
+				<td>{{ $todo->created_at }}</td>
+				<td><a href="#"> read more </a></td>
+			</tr>
+
+		@empty
+			<h4> nah </h4>
+		@endforelse
+
+	</tbody>
+</table>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TodoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::resource('todo', TodoController::class);
